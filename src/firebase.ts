59ba2f6c42b46d5ad7d9c077e5f8f16dd9be74d4
Pedro Donato <pedrohatake755@gmail.com>
2026-04-17 11:42:37 -0300

    fix firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import config from '../firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const app = initializeApp(config);
+const firebaseConfig = {
+  apiKey: "AIzaSyCANUYXEamJhcWum1OiWFedUQ0hdYd73yg",
+  authDomain: "gen-lang-client-0623061646.firebaseapp.com",
+  projectId: "gen-lang-client-0623061646",
+  storageBucket: "gen-lang-client-0623061646.firebasestorage.app",
+  messagingSenderId: "372496366667",
+  appId: "1:372496366667:web:8f30f87586542d280a61ce"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, config.firestoreDatabaseId);
-export const googleProvider = new GoogleAuthProvider();
